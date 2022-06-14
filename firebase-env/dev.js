28711bc2adb2a6934d2b8f9b2629a67799e16d10
Nicolas Rio <nicolas.rio42@gmail.com>
2022-06-14 21:03:21 +0200

    deploy script

--- firebase-env/dev.js
+++ firebase-env/dev.js
@@ -1,22 +1,22 @@
 // DEV
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBTei7EpZgxIZuHFOZBhuQXSyCaaqo_rdg",
-//   authDomain: "routechoice-db-dev.firebaseapp.com",
-//   projectId: "routechoice-db-dev",
-//   storageBucket: "routechoice-db-dev.appspot.com",
-//   messagingSenderId: "215455261611",
-//   appId: "1:215455261611:web:13cab2dd06e508e0bc68ee",
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyBTei7EpZgxIZuHFOZBhuQXSyCaaqo_rdg",
+  authDomain: "routechoice-db-dev.firebaseapp.com",
+  projectId: "routechoice-db-dev",
+  storageBucket: "routechoice-db-dev.appspot.com",
+  messagingSenderId: "215455261611",
+  appId: "1:215455261611:web:13cab2dd06e508e0bc68ee",
+};
 
 // PROD
-const firebaseConfig = {
-  apiKey: "AIzaSyBiSUtthG2k9rv7X1V40RfZ9WmyiqvmsYM",
-  authDomain: "routechoice-db.firebaseapp.com",
-  projectId: "routechoice-db",
-  storageBucket: "routechoice-db.appspot.com",
-  messagingSenderId: "637850605319",
-  appId: "1:637850605319:web:fee21eab46161cd21c97a2",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBiSUtthG2k9rv7X1V40RfZ9WmyiqvmsYM",
+//   authDomain: "routechoice-db.firebaseapp.com",
+//   projectId: "routechoice-db",
+//   storageBucket: "routechoice-db.appspot.com",
+//   messagingSenderId: "637850605319",
+//   appId: "1:637850605319:web:fee21eab46161cd21c97a2",
+// };
 
 export default firebaseConfig;
