ec68d5f31cb3e30e662276e7c94e2f8608ea3128
Nicolas Rio <nicolas.rio42@gmail.com>
2022-12-05 14:02:56 +0100

    Improving deploy script

--- deploy.js
+++ deploy.js
@@ -1,31 +1,40 @@
-import { exec } from "child_process";
+import { execSync } from "child_process";
 import { copyFileSync } from "fs";
 import { join } from "path";
 
 const argEnv = process.argv[2];
-const restArgs = process.argv.slice(2);
+const restArgs = process.argv.slice(3);
 const environment = getEnvironmentFromArgumentOrThowError(argEnv);
 
 const targetEnvironmentFile = join(
   "environments",
-  `environments.${environment}.js`
+  `environment.${environment}.js`
 );
 
 const devEnvironmentFile = join("environments", "environment.dev.js");
 const environmentFile = join("environments", "environment.js");
 
-exec(`firebase use ${environment}`);
+console.log(`firebase use ${environment}`);
+execSync(`firebase use ${environment}`);
+
+console.log(`Copying environment.${environment}.js to environment.js`);
 copyFileSync(targetEnvironmentFile, environmentFile);
 
-if (
-  (restArgs[0] === "--only" && restArgs[1].includes("hosting")) ||
-  restArgs.length === 0
-)
-  exec("npm run build");
+const onlyArgIndex = restArgs.findIndex((a) => a === "--only");
 
-exec(`firebase deploy ${restArgs.join(" ")}`);
+if (onlyArgIndex === -1 || restArgs[onlyArgIndex + 1]?.includes("hosting")) {
+  console.log("Building application");
+  execSync("npm run build");
+}
+
+console.log(`firebase deploy ${restArgs.join(" ")}`);
+execSync(`firebase deploy ${restArgs.join(" ")}`);
+
+console.log("Copying environment.dev.js to environment.js");
 copyFileSync(devEnvironmentFile, environmentFile);
-exec("firebase use dev");
+
+console.log("firebase use dev");
+execSync("firebase use dev");
 
 function getEnvironmentFromArgumentOrThowError(environmentFromArgument) {
   if (environmentFromArgument === "dev") return "dev";
