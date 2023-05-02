e46b1d22288d0b408ea4f0bb8a44c085717dca94
Nicolas Rio <nicolas.rio42@gmail.com>
2023-05-02 17:29:13 +0200

    Fixing deploy script

---
+++ apps/web-client/deploy.js
@@ -0,0 +1,41 @@
+import { execSync } from 'child_process';
+import { copyFileSync } from 'fs';
+import { join } from 'path';
+
+const argEnv = process.argv[2];
+const restArgs = process.argv.slice(3);
+const environment = getEnvironmentFromArgumentOrThowError(argEnv);
+
+const targetEnvironmentFile = join('src', 'environments', `environment.${environment}.js`);
+
+const devEnvironmentFile = join('src', 'environments', 'environment.dev.js');
+const environmentFile = join('src', 'environments', 'environment.js');
+
+console.log(`firebase use ${environment}`);
+execSync(`firebase use ${environment}`);
+
+console.log(`Copying environment.${environment}.js to environment.js`);
+copyFileSync(targetEnvironmentFile, environmentFile);
+
+const onlyArgIndex = restArgs.findIndex((a) => a === '--only');
+
+if (onlyArgIndex === -1 || restArgs[onlyArgIndex + 1]?.includes('hosting')) {
+	console.log('Building application');
+	execSync('npm run build');
+}
+
+console.log(`firebase deploy ${restArgs.join(' ')}`);
+execSync(`firebase deploy ${restArgs.join(' ')}`);
+
+console.log('Copying environment.dev.js to environment.js');
+copyFileSync(devEnvironmentFile, environmentFile);
+
+console.log('firebase use dev');
+execSync('firebase use dev');
+
+function getEnvironmentFromArgumentOrThowError(environmentFromArgument) {
+	if (environmentFromArgument === 'dev') return 'dev';
+	if (environmentFromArgument === 'staging') return 'staging';
+	if (environmentFromArgument === 'prod') return 'prod';
+	throw new Error('Unknown environment');
+}
