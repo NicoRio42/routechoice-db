03f47264fd6c5b8208354ec566e79a741333ec69
Nicolas Rio <nicolas.rio@cgi.com>
2023-09-25 09:02:14 +0200

    Env vars from env object

--- scripts/migrate-prod.js
+++ scripts/migrate-prod.js
@@ -1,12 +1,15 @@
 import { drizzle } from 'drizzle-orm/libsql';
 import { migrate } from 'drizzle-orm/libsql/migrator';
 import { createClient } from '@libsql/client/web';
-import { TURSO_DB_TOKEN } from '$env/static/private';
 
+/**
+ * @typedef {Object} Env
+ * @property {string} TURSO_DB_TOKEN
+ */
 
 const db = drizzle(createClient({
     url: 'libsql://routechoice-db-routechoice-db.turso.io',
-    authToken: TURSO_DB_TOKEN,
+    authToken: env.TURSO_DB_TOKEN,
 }));
 
 migrate(db, { migrationsFolder: './migrations' });
