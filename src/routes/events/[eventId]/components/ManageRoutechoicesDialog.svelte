f9d9720ba54ed81e71e85e3849bec6b75093ad02
Nicolas Rio <nicolas.rio@cgi.com>
2023-10-08 10:35:59 +0200

    Modal support on ios

--- src/routes/events/[eventId]/components/ManageRoutechoicesDialog.svelte
+++ src/routes/events/[eventId]/components/ManageRoutechoicesDialog.svelte
@@ -2,19 +2,14 @@
 	import { enhance } from "$app/forms";
 	import type { Routechoice } from "$lib/server/db/schema.js";
 	import { createEventDispatcher } from "svelte";
+	import { fade } from "svelte/transition";
 
     export let routechoices: Routechoice[];
     export let eventId: string;
     export let show: boolean;
     
     let loading = false;
-    let dialog: HTMLDialogElement;
     const dispatch = createEventDispatcher<{startDrawingNewRoutechoice: undefined}>();
-
-    $: {
-        if (show) dialog?.showModal()
-        else dialog?.close()
-    }
 
 	function confirmDeletion(e: Event) {
 		if (!confirm('Are you sure to delete this routechoice?')) {
@@ -25,57 +20,60 @@
 	}
 </script>
 
-<dialog bind:this={dialog}>
-	<article>
-        <h2>Routechoices</h2>
+<!-- Dialog showModal and close methods are not well supported on IOS -->
+{#if show}
+    <dialog open transition:fade={{duration: 125}}>
+        <article>
+            <h2>Routechoices</h2>
 
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Length</th>
-                    <th>Color</th>
-                    <th></th>
-                </tr>
-            </thead>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Length</th>
+                        <th>Color</th>
+                        <th></th>
+                    </tr>
+                </thead>
 
-            <tbody>
-                {#each routechoices as routechoice (routechoice.id)}
-                <tr>
-                    <td>{routechoice.name}</td>
-                    <td>{Math.round(routechoice.length)} m</td>
-                    <td style:background-color={routechoice.color}></td>
+                <tbody>
+                    {#each routechoices as routechoice (routechoice.id)}
+                    <tr>
+                        <td>{routechoice.name}</td>
+                        <td>{Math.round(routechoice.length)} m</td>
+                        <td style:background-color={routechoice.color}></td>
 
-                    <td>
-                        <form action="/events/{eventId}/legs/{routechoice.fkLeg}/routechoices/{routechoice.id}?/delete"
-                            method="post"
-                            on:submit={confirmDeletion}
-                            use:enhance={() => {
-                                loading = true;
-                                return ({update}) => {
-                                    loading = false;
-                                    show = false;
-                                    update();
-                                }
-                            }}
-                            class="m-0 p-0"
-                        >
-                            <button type="submit" class="btn-unset" aria-busy={loading}>
-                                <i class="i-carbon-trash-can w-5 h-5 block" />
-                            </button>
-                        </form>
-                    </td>
-                </tr>
-                    
-                {/each}
-            </tbody>
-        </table>
+                        <td>
+                            <form action="/events/{eventId}/legs/{routechoice.fkLeg}/routechoices/{routechoice.id}?/delete"
+                                method="post"
+                                on:submit={confirmDeletion}
+                                use:enhance={() => {
+                                    loading = true;
+                                    return ({update}) => {
+                                        loading = false;
+                                        show = false;
+                                        update();
+                                    }
+                                }}
+                                class="m-0 p-0"
+                            >
+                                <button type="submit" class="btn-unset" aria-busy={loading}>
+                                    <i class="i-carbon-trash-can w-5 h-5 block" />
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                        
+                    {/each}
+                </tbody>
+            </table>
 
-        <button type="button" on:click={() => {
-            dispatch("startDrawingNewRoutechoice")
-            show = false;
-        }}>Add routechoice</button>
+            <button type="button" on:click={() => {
+                dispatch("startDrawingNewRoutechoice")
+                show = false;
+            }}>Add routechoice</button>
 
-        <button type="button" class="outline" on:click={() => show = false}>Cancel</button>
-    </article>
-</dialog>
+            <button type="button" class="outline" on:click={() => show = false}>Cancel</button>
+        </article>
+    </dialog>
+{/if}
