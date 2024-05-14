75049bcd55ec295274ead52750811bc02ef45ca7
Nicolas Rio <nicolas.rio42@gmail.com>
2024-05-14 17:29:04 +0200

    Updating prod migration script

--- scripts/migrate-prod.js
+++ scripts/migrate-prod.js
@@ -1,11 +1,13 @@
 import { drizzle } from 'drizzle-orm/libsql';
 import { migrate } from 'drizzle-orm/libsql/migrator';
 import { createClient } from '@libsql/client/web';
+import { env } from 'node:process';
 
+const db = drizzle(
+	createClient({
+		url: 'libsql://routechoice-db-routechoice-db.turso.io',
+		authToken: env.TURSO_DB_TOKEN
+	})
+);
 
-const db = drizzle(createClient({
-    url: 'libsql://routechoice-db-routechoice-db.turso.io',
-    authToken: "",
-}));
-
-migrate(db, { migrationsFolder: './migrations' });
+await migrate(db, { migrationsFolder: './migrations' });
